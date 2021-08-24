f9e7e9eb43539f7edb26c0d40593ba4662f92c0e
ritik1305 <ritik1305@gmail.com>
2021-08-24 09:04:50 +0530

    Final one

--- lib/screens/add_movie_screen.dart
+++ lib/screens/add_movie_screen.dart
@@ -1,133 +1,133 @@
-import 'package:flutter/material.dart';
-import 'package:mywatchlist/helpers/database_helper.dart';
-import 'package:mywatchlist/models/task_model.dart';
+// import 'package:flutter/material.dart';
+// import 'package:mywatchlist/helpers/database_helper.dart';
+// import 'package:mywatchlist/models/task_model.dart';
 
-class AddMovieScreen extends StatefulWidget {
-  final Function updateTaskList;
-  final Task task;
+// class AddMovieScreen extends StatefulWidget {
+//   final Function updateTaskList;
+//   final Task task;
 
-  AddMovieScreen({required this.updateTaskList, required this.task});
+//   AddMovieScreen({required this.updateTaskList, required this.task});
 
-  //const AddMovieScreen({Key? key}) : super(key: key);
+//   //const AddMovieScreen({Key? key}) : super(key: key);
 
-  @override
-  _AddMovieScreenState createState() => _AddMovieScreenState();
-}
+//   @override
+//   _AddMovieScreenState createState() => _AddMovieScreenState();
+// }
 
-class _AddMovieScreenState extends State<AddMovieScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String _title = '';
-  String _director = '';
+// class _AddMovieScreenState extends State<AddMovieScreen> {
+//   final _formKey = GlobalKey<FormState>();
+//   String _title = '';
+//   String _director = '';
 
-  _submit() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      print('$_title, $_director');
+//   _submit() {
+//     if (_formKey.currentState!.validate()) {
+//       _formKey.currentState!.save();
+//       print('$_title, $_director');
 
-      Task task = Task(title: _title, director: _director);
-      if (widget.task == null) {
-        Task.status = 0;
-        DatabaseHelper.instance.insertTask(task);
-      } else {
-        task.status = widget.task.status;
-        DatabaseHelper.instance.updateTask(task);
-      }
+//       Task task = Task(title: _title, director: _director);
+//       if (widget.task == null) {
+//         Task.status = 0;
+//         DatabaseHelper.instance.insertTask(task);
+//       } else {
+//         task.status = widget.task.status;
+//         DatabaseHelper.instance.updateTask(task);
+//       }
 
-      widget.updateTaskList();
-      Navigator.pop(context);
-    }
-  }
+//       widget.updateTaskList();
+//       Navigator.pop(context);
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () => Navigator.pop(context),
-            child: Icon(
-              Icons.arrow_back_ios,
-              size: 30.0,
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            'Add Text',
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  child: TextFormField(
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                      labelStyle: TextStyle(fontSize: 18.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    validator: (input) =>
-                        input!.trim().isEmpty ? 'Please enter a title' : null,
-                    onSaved: (input) => _title = input!,
-                    initialValue: _title,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  child: TextFormField(
-                    style: TextStyle(fontSize: 18.0),
-                    decoration: InputDecoration(
-                      labelText: 'Director',
-                      labelStyle: TextStyle(fontSize: 18.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    validator: (input) => input!.trim().isEmpty
-                        ? 'Please enter Director name'
-                        : null,
-                    onSaved: (input) => _director = input!,
-                    initialValue: _director,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 20.0),
-                  height: 60.0,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  child: TextButton(
-                    child: Text(
-                      'Add',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    onPressed: _submit,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           GestureDetector(
+//             onTap: () => Navigator.pop(context),
+//             child: Icon(
+//               Icons.arrow_back_ios,
+//               size: 30.0,
+//               color: Theme.of(context).primaryColor,
+//             ),
+//           ),
+//           SizedBox(
+//             height: 20.0,
+//           ),
+//           Text(
+//             'Add Text',
+//             style: TextStyle(
+//                 color: Colors.black,
+//                 fontSize: 40.0,
+//                 fontWeight: FontWeight.bold),
+//           ),
+//           SizedBox(
+//             height: 10.0,
+//           ),
+//           Form(
+//             key: _formKey,
+//             child: Column(
+//               children: [
+//                 Padding(
+//                   padding: EdgeInsets.symmetric(vertical: 20.0),
+//                   child: TextFormField(
+//                     style: TextStyle(fontSize: 18.0),
+//                     decoration: InputDecoration(
+//                       labelText: 'Title',
+//                       labelStyle: TextStyle(fontSize: 18.0),
+//                       border: OutlineInputBorder(
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                     ),
+//                     validator: (input) =>
+//                         input!.trim().isEmpty ? 'Please enter a title' : null,
+//                     onSaved: (input) => _title = input!,
+//                     initialValue: _title,
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.symmetric(vertical: 20.0),
+//                   child: TextFormField(
+//                     style: TextStyle(fontSize: 18.0),
+//                     decoration: InputDecoration(
+//                       labelText: 'Director',
+//                       labelStyle: TextStyle(fontSize: 18.0),
+//                       border: OutlineInputBorder(
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                     ),
+//                     validator: (input) => input!.trim().isEmpty
+//                         ? 'Please enter Director name'
+//                         : null,
+//                     onSaved: (input) => _director = input!,
+//                     initialValue: _director,
+//                   ),
+//                 ),
+//                 Container(
+//                   margin: EdgeInsets.symmetric(vertical: 20.0),
+//                   height: 60.0,
+//                   width: double.infinity,
+//                   decoration: BoxDecoration(
+//                     color: Theme.of(context).primaryColor,
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                   child: TextButton(
+//                     child: Text(
+//                       'Add',
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 20.0,
+//                       ),
+//                     ),
+//                     onPressed: _submit,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
